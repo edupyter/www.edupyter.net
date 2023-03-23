11502ce9abdf0fc134dfbfb1faea5d93cb81711b
Laurent Abbal <laurent@abbal.com>
2023-03-24 08:38:13 +0900

    v1.10

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 				<?php
 				$version_38 = "1.10";
 				$version_39 = "1.10";
-				$version_310 = "1.9";
+				$version_310 = "1.10";
 				$download_38 =  "https://github.com/edupyter/EDUPYTER38/releases/download/v".$version_38."/EDUPYTER-38-".$version_38."-setup.exe";
 				$download_39 =  "https://github.com/edupyter/EDUPYTER39/releases/download/v".$version_39."/EDUPYTER-39-".$version_39."-setup.exe";
 				$download_310 = "https://github.com/edupyter/EDUPYTER310/releases/download/v".$version_310."/EDUPYTER-310-".$version_310."-setup.exe";
