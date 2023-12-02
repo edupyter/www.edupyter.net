0ac2dcc29eb9b457a3078f32d1465190e3d14483
Laurent Abbal <laurent@abbal.com>
2023-12-02 15:19:54 +0900

    ~

--- inc.versions.php
+++ inc.versions.php
@@ -2,7 +2,7 @@
 $version_38 = "3.8.10";
 $version_39 = "3.9.13";
 $version_310 = "3.10.11";
-$version_311 = "3.11.4";
+$version_311 = "3.11.6";
 $version_312 = "3.12.0";
 $download_38 =  "https://github.com/edupyter/EDUPYTER38/releases/download/v".$version_38."/EDUPYTER-38-".$version_38."-setup.exe";
 $download_39 =  "https://github.com/edupyter/EDUPYTER39/releases/download/v".$version_39."/EDUPYTER-39-".$version_39."-setup.exe";
