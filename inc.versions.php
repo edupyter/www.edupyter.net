2e65c8683b9aedaa997593eaa6c7e7c5d026b92f
Laurent Abbal <laurent@abbal.com>
2024-03-26 10:38:42 +0900

    maj

--- inc.versions.php
+++ inc.versions.php
@@ -1,12 +1,19 @@
 <?php
+$release_38 = "3.8.10-2";
+$release_39 = "3.9.13-2";
+$release_310 = "3.10.11-2";
+$release_311 = "3.11.6";
+$release_312 = "3.12.0";
+
 $version_38 = "3.8.10";
 $version_39 = "3.9.13";
 $version_310 = "3.10.11";
 $version_311 = "3.11.6";
 $version_312 = "3.12.0";
-$download_38 =  "https://github.com/edupyter/EDUPYTER38/releases/download/v".$version_38."/EDUPYTER-38-".$version_38."-setup.exe";
-$download_39 =  "https://github.com/edupyter/EDUPYTER39/releases/download/v".$version_39."/EDUPYTER-39-".$version_39."-setup.exe";
-$download_310 = "https://github.com/edupyter/EDUPYTER310/releases/download/v".$version_310."/EDUPYTER-310-".$version_310."-setup.exe";
-$download_311 = "https://github.com/edupyter/EDUPYTER311/releases/download/v".$version_311."/EDUPYTER-311-".$version_311."-setup.exe";
-$download_312 = "https://github.com/edupyter/EDUPYTER312/releases/download/v".$version_312."/EDUPYTER-312-".$version_312."-setup.exe";
+
+$download_38 =  "https://github.com/edupyter/EDUPYTER38/releases/download/v".$release_38."/EDUPYTER-38-".$version_38."-setup.exe";
+$download_39 =  "https://github.com/edupyter/EDUPYTER39/releases/download/v".$release_39."/EDUPYTER-39-".$version_39."-setup.exe";
+$download_310 = "https://github.com/edupyter/EDUPYTER310/releases/download/v".$release_310."/EDUPYTER-310-".$version_310."-setup.exe";
+$download_311 = "https://github.com/edupyter/EDUPYTER311/releases/download/v".$release_311."/EDUPYTER-311-".$version_311."-setup.exe";
+$download_312 = "https://github.com/edupyter/EDUPYTER312/releases/download/v".$release_312."/EDUPYTER-312-".$version_312."-setup.exe";
 ?>
